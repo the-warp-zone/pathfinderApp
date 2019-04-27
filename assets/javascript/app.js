1336cd5a07bb01d53e6cc32c3abdd4984c838789
CalebRose <ctrose17@gmail.com>
2019-04-27 10:00:23 -0500

    Quality changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-// Variables
+// Variables 
 var map;
 var map2;
 var mapQuestLayer;
@@ -6,13 +6,35 @@ let startInput = "";
 let endInput = "";
 var googleBool = false;
 var mapQuestBool = false;
+let currentU;
+let user;
 
+// Firebase Authentication
+let promise1 = new Promise(function (resolve, reject) {
+  firebase.auth().onAuthStateChanged(user => {
+    if (!user) {
+      window.location = "index.html";
+      //If User is not logged in , redirect to login page
+    } else {
+      resolve(user);
+    }
+  });
+});
+
+// Grabs User ID (uid)
+promise1.then(function (value) {
+  currentU = value.uid;
+  user = value;
+});
+
+// If user attempts to access application without loggin in...
 firebase.auth().onAuthStateChanged(user => {
   if (!user) {
-    window.location = "index.html"; //If User is not logged in, redirect to login page
+    window.location = "index.html"; // Redirects to login page
   }
 });
 
+// Logout
 document.getElementById("logoutBtn").addEventListener("click", fbaseLogout);
 document
   .getElementById("mobileLogoutBtn")
@@ -22,7 +44,10 @@ function fbaseLogout() {
   firebase.auth().signOut();
 }
 
+// Submit Click
 $(document).on("click", "#submit", submitFunction);
+
+// Clicking on the "G"
 $(document).on("click", "#google-link", function () {
   if (googleBool) {
     var googleURL =
@@ -35,6 +60,7 @@ $(document).on("click", "#google-link", function () {
   }
 });
 
+// Clicking on the "M"
 $(document).on("click", "#mapQuest-link", function () {
   if (mapQuestBool) {
     var mapQuestURL =
@@ -59,6 +85,7 @@ function submitFunction() {
   event.preventDefault();
   startInput = $("#starting-location").val();
   endInput = $("#ending-location").val();
+  writeUserData(currentU);
   initMap();
   calculateRoute(startInput, endInput);
   calculateRoute2(startInput, endInput);
@@ -133,6 +160,7 @@ function calculateRoute(start, end) {
     myOptions
   );
 
+  // Call Directions API
   var directionsService = new google.maps.DirectionsService();
   var directionsRequest = {
     origin: start,
@@ -150,6 +178,7 @@ function calculateRoute(start, end) {
   });
 }
 
+// Call MapQuest API
 function calculateRoute2() {
   L.mapquest.directions().route({
       start: startInput,
@@ -171,31 +200,21 @@ function calculateRoute2() {
   );
 }
 
+// Mapquest Key
 L.mapquest.key = "C1a3TOmczQOtn6JOIApQAx3vJ3S20kF0";
 
+// Displays MapQuest Map
 map2 = L.mapquest.map("hybrid", {
   center: [30.266926, -97.750519],
   layers: L.mapquest.tileLayer("hybrid"),
   zoom: 12
 });
 
+// Firebase Code
+var database = firebase.database();
 
-
-// var database = firebase.database();
-// // write
-// database.ref('users/' + userId).set(user);
-
-// // read / listen
-// database.child("users").on('value', function (snapshot) {
-//   $(document).on("click", "#submit", submitFunction);
-
-//   function writeUserData(userId, startInput, endInput, initMap(calculateRoute, calculateRoute2)) {
-//     firebase.database().ref('users/' + userId).set({
-//       startInput: startInput,
-//       endInput: endInput,
-//       initMap: calculateRoute,
-//       calculateRoute2,
-//       database.ref('users/' + userId).set(snapshot);
-//     });
-//   }
-// });
+function writeUserData(currentU) {
+  firebase.database().ref('users/' + currentU).push({
+    Inputs: [startInput, endInput],
+  });
+}
